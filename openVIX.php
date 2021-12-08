97b27f158c7c03f57b946971832e4219fbc98ba4
Guy Sinter <guy.s@tomadox.co.uk>
2021-12-08 11:43:25 +0200

    remove code comments

--- openVIX.php
+++ openVIX.php
@@ -67,7 +67,6 @@ if (has_capability("mod/quiz:attempt", $context)) {
             $token = $tokenrequest["data"]["token"];
             $parid = $tokenrequest["data"]["parID"];
             $url = "vix://?examCode=$code&token=$token&parID=$parid&thirdPartyStartupURL=$logintopanel";
-            // echo $url;
             header("location: $url");
             exit;
         }
