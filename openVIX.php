9dec65eb1730cf6d87cff1840d4d815d93e63a9d
Guy Sinter <guy.s@tomadox.co.uk>
2021-12-08 11:22:32 +0200

    3 slashes comments are not allowed

--- openVIX.php
+++ openVIX.php
@@ -26,7 +26,7 @@ require_login();
 require_once($CFG->dirroot . "/mod/quiz/accessrule/tomaetest/rule.php");
 
 if (!defined('MOODLE_INTERNAL')) {
-    die('Direct access to this script is forbidden.'); /// It must be included from a Moodle page
+    die('Direct access to this script is forbidden.'); // It must be included from a Moodle page.
 }
 
 
