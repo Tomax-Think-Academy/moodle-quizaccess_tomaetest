dc2941e620eaded2cb67d53e4423b4209d37cb49
Guy Sinter <guy.s@tomadox.co.uk>
2021-12-08 10:15:55 +0200

    Expected MOODLE_INTERNAL check

--- db/tasks.php
+++ db/tasks.php
@@ -21,6 +21,7 @@
  * @copyright  2021 Tomax ltd <roy@tomax.co.il>
  * @license    http://www.gnu.org/copyleft/gpl.html GNU GPL v3 or later
  */
+defined('MOODLE_INTERNAL') || die();
 $tasks = array(
     array(
         'classname' => 'quizaccess_tomaetest\task\close_etest_quiz',
