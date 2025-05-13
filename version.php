382bfa323144bc8aa03a22657d23120aa883fae5
Ron <ron.peker@tomax.io>
2025-05-13 08:25:26 +0300

    adding general setting to include only users enrolled in the course

--- version.php
+++ version.php
@@ -22,7 +22,7 @@
  * @license    http://www.gnu.org/copyleft/gpl.html GNU GPL v3 or later
  */
 defined('MOODLE_INTERNAL') || die();
-$plugin->version = 2025031900;
+$plugin->version = 2025051300;
 $plugin->requires = 2010042803;
 $plugin->component = 'quizaccess_tomaetest';
 $plugin->maturity = MATURITY_STABLE;
